8093037de3c34be7c270f96d4aef572bc6a4d477
0jimah <godfreyakuba@gmail.com>
2022-09-06 11:03:30 +0200

    restaurant card added

---
+++ components/DishRow.js
@@ -0,0 +1,19 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import Currency from "react-currency-formatter";
+
+const DishRow = ({ id, name, description, price, image }) => {
+  return (
+    <TouchableOpacity>
+      <View>
+        <Text className="text-lg  mb-1">{name}</Text>
+        <Text className="text-gray-400">{description}</Text>
+        <Text className="text-gray-400 mt-2">
+          <Currency quantity={price} currency="EUR" />
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default DishRow;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,15 @@
-import { View, Text, Image, TextInput } from "react-native";
+import { View, Text, Image, TextInput, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import * as Icons from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
+import sanityClient from "../sanity";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -13,10 +17,28 @@ const HomeScreen = () => {
     });
   }, []);
 
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `
+     *[_type == 'featured']{
+       ...,
+       restaurants[]->{
+        ...,
+        dishes[]->
+       }
+     }
+    `
+      )
+      .then((data) => {
+        setFeaturedCategories(data);
+      });
+  }, []);
+
   return (
     <SafeAreaView className="bg-white pt-5">
       {/* Header */}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+      <View className="flex-row pb-2 items-center mx-4 space-x-2">
         <Image
           source={{
             uri: "https://links.papareact.com/wru",
@@ -35,18 +57,34 @@ const HomeScreen = () => {
 
       {/* search */}
       <View className="flex-row items-center space-x-2 pb-2 mx-4 ">
-        <View className="flex-row flex-1 items-center  min-w-0 space-x-2 justify-end  bg-gray-200 p-3 mr-9">
+        <View className="flex-row flex-1 space-x-2  bg-gray-200 p-3">
           <Icons.MagnifyingGlassIcon color="gray" size={20} />
           <TextInput
             placeholder="Restaurants and cuisines"
             keyboardType="default"
           />
         </View>
-        <Icons.AdjustmentsVerticalIcon
-          className="flex-auto ml-6"
-          color="#00ccbb"
-        />
+        <Icons.AdjustmentsVerticalIcon color="#00ccbb" />
       </View>
+
+      {/*Body  */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        {/* Catagories */}
+        <Categories />
+        {/* Feautered rows */}
+
+        {featuredCategories?.map((category) => (
+          <FeaturedRow
+            key={category._id}
+            id={category._id}
+            title={category.name}
+            description={category.short_description}
+          />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
