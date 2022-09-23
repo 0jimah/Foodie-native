5b7ef9c740de19b8cf7e594a9e79fe51c9610e81
0jimah <godfreyakuba@gmail.com>
2022-09-23 11:02:00 +0200

    map added

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,33 +1,134 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from 'react-native';
 import React, { useMemo } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
-import { selectBasketItems } from '../features/basketSlice';
+import {
+  removeFromBasket,
+  selectBasketItems,
+  selectBasketTotal,
+} from '../features/basketSlice';
+import * as Icons from 'react-native-heroicons/outline';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import Currency from 'react-currency-formatter';
+import { urlFor } from '../sanity';
 
 const BasketScreen = () => {
   const navigation = useNavigation();
   const restaurant = useSelector(selectRestaurant);
   const items = useSelector(selectBasketItems);
   const [groupItemsInBasket, setGroupItemsInBasket] = useState([]);
+  const basketTotal = useSelector(selectBasketTotal);
 
   const dispatch = useDispatch();
 
-  useMemo(() => {
+  useEffect(() => {
     const groupItems = items.reduce((results, item) => {
-      (results[items.id] = results[items.id] || []).push(item);
+      (results[item.id] = results[item.id] || []).push(item);
       return results;
     }, {});
     setGroupItemsInBasket(groupItems);
   }, [items]);
 
-  console.log(groupItemsInBasket);
-
   return (
-    <View>
-      <Text>BasketScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 pt-7 border-b border-[#00ccbb] bg-white shadow-xs">
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-center text-gray-400">
+              {restaurant.title}
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full bg-gray-100 absolute top-3 right-5"
+          >
+            <Icons.XCircleIcon color="#00ccbb" height={50} width={50} />
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex-row items-center space-x-4 px-4 py-3 bg-white my-5">
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru',
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <Text className="flex-1">Deliver in 50-75 min</Text>
+          <TouchableOpacity>
+            <Text className="text-[#00ccbb]">Change</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView className="divide-y divide-gray-200">
+          {Object.entries(groupItemsInBasket).map(([key, items]) => (
+            <View
+              key={key}
+              className="flex-row items-center space-x-3 bg-white py-2 px-5"
+            >
+              <Text className="text-[#00ccbb]">{items.length} x</Text>
+              <Image
+                source={{ uri: urlFor(items[0]?.image).url() }}
+                className="h-12 w-12 rounded-full"
+              />
+              <Text className="flex-1">{items[0]?.name}</Text>
+              <Text className="text-gray-600">
+                <Currency quantity={items[0]?.price} currency="EUR" />
+              </Text>
+
+              <TouchableOpacity>
+                <Text
+                  className="text-[#00ccbb] text-xs"
+                  onPress={() => dispatch(removeFromBasket({ id: key }))}
+                >
+                  Remove
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+
+        <View className="p-5 bg-white mt-5 space-y-4">
+          <View className="flex-row justify-between">
+            <Text className="text-gray-400">Subtotal</Text>
+            <Text className="text-gray-400">
+              <Currency quantity={basketTotal} currency="EUR" />
+            </Text>
+          </View>
+          <View className="flex-row justify-between">
+            <Text className="text-gray-400">Delivery Fee</Text>
+            <Text className="text-gray-400">
+              <Currency quantity={1.99} currency="EUR" />
+            </Text>
+          </View>
+          <View className="flex-row justify-between">
+            <Text>Order Total</Text>
+            <Text className="font-extrabold">
+              <Currency quantity={basketTotal + 1.99} currency="EUR" />
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate('PreparingOrderScreen')}
+            className="rounded-lg bg-[#00ccbb] p-3"
+          >
+            <Text className="text-center text-white text-lg font-bold">
+              Place Order
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
